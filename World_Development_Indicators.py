99aa93084666f56c0aa606995886efbb7d88589f
cathylyirang <yirang@admins-MacBook-Pro.local>
2024-09-11 15:11:19 -0400

    this is our second commit

---
+++ World_Development_Indicators.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+content = pd.read_csv(
+    "https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+)
+selected_columns = content[
+    [
+        "Mortality rate, infant (per 1,000 live births)",
+        "GDP per capita (constant 2010 US$)",
+        "Country Name",
+    ]
+]
+
+x = selected_columns["Mortality rate, infant (per 1,000 live births)"]
+y = selected_columns["GDP per capita (constant 2010 US$)"]
+
+plt.scatter(x, y)
+# fig, ax = plt.subplots()
+# ax.plot(selected_columns)
+plt.xlabel("GDP per Capita")
+plt.ylabel("Mortality rate")
+plt.show()
